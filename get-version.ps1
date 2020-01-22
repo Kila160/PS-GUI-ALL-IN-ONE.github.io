9c4331137b004e02a2fcdb29af402c07c02c040c
Bruno Silva <60047796+Kila160@users.noreply.github.com>
2020-01-22 13:52:05 +0000

    Alteração do desc do file
    Alteração desc do file

---
+++ get-version.ps1
@@ -0,0 +1,5 @@
+﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+#$WebResponseObj = Invoke-WebRequest -Uri "https://kila160.github.io/PS-GUI-ALL-IN-ONE.github.io/version.xml"
+#$WebResponseObj.content
+
+$HTML = Invoke-RestMethod 'https://kila160.github.io/PS-GUI-ALL-IN-ONE.github.io/version.xml' $HTML -match 'Tove' True
